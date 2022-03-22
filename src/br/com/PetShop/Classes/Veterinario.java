a990fb2e37aa43b9a8fda2b3b230320d0d56e459
Leone Oliveira <leone.farias@sensedia.com>
2022-03-22 15:07:38 -0300

    modificando o nome da variavel cmrv para o correto crmv

--- src/br/com/PetShop/Classes/Veterinario.java
+++ src/br/com/PetShop/Classes/Veterinario.java
@@ -2,14 +2,14 @@ package br.com.PetShop.Classes;
 
 public class Veterinario extends Funcionario{
 
-    private String cmrv;
+    private String crmv;
 
-    public Veterinario(int codigo, String nome, String sobrenome, int cpf, String sexo, double salario, String cmrv) {
+    public Veterinario(int codigo, String nome, String sobrenome, int cpf, String sexo, double salario, String crmv) {
         super(codigo, nome, sobrenome, cpf, sexo, salario);
-        this.cmrv = cmrv;
+        this.crmv = crmv;
     }
 
     public String getCmrv() {
-        return cmrv;
+        return crmv;
     }
 }
